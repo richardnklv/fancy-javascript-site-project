a1f99b14548609dfd0dcd76baa5a26e38a1a4ba7
richardnklv <richardnklv@outlook.com>
2024-03-25 18:47:02 +0100

    Hero section added, with an image, sections, lines, circles, blurs, fancy css.

--- src/components/design/Header.jsx
+++ src/components/design/Header.jsx
@@ -31,7 +31,7 @@ export const BackgroundCircles = () => {
 export const HamburgerMenu = () => {
   return (
     <div className="absolute inset-0 pointer-events-none lg:hidden">
-      <div className="absolute inset-0 opacity-[.03]">
+      <div className="absolute inset-0 opacity-[.08]">
         <img
           className="w-full h-full object-cover"
           src={background}
